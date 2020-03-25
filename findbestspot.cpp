a68a97cbcb510136f2f0a5eef7fa04018457e301
nsamu <nsamu@10.7.178.56>
2020-03-25 13:13:17 -0400

    maybe?

--- findbestspot.cpp
+++ findbestspot.cpp
@@ -6,19 +6,22 @@
  */
 #include "playGame.cpp"
 
-//int findbestspoty(int currx, int curry, int &bestx, int &besty, GameBoard *game, int oldct);
-//int findbestspot(int currx, int curry,int &bestx,int &besty,GameBoard *game,int oldct);
-
-int *currcount = 0;
-int *newx = 0;
-int *newy = 0;
+int findbestspoty(int currx, int curry, int &bestx, int &besty, GameBoard *game, int oldct);
+int findbestspot(int currx, int curry,int &bestx,int &besty,GameBoard *game,int oldct);
 
 int findbestspot(int currx, int curry,int &bestx,int &besty,GameBoard *game,int oldct){
-
+	if(currx + 1 == game-> size){
+			return oldct;
+	}
+	findbestspoty(currx, curry, bestx, besty, game, oldct);
+	findbestspot(currx + 1, curry, bestx, besty, game, oldct);
 
 }
 int findbestspoty(int currx, int curry, int &bestx, int &besty, GameBoard *game, int oldct){
 	int ct = 0;
+	if(game->board[currx][curry] == 'X' || game->board[currx][curry] == 'O'){
+		return oldct;
+	}
 	shouldweflip(currx, curry, -1, -1, game, ct);
 	shouldweflip(currx, curry, -1,  0, game, ct);
 	shouldweflip(currx, curry, -1,  1, game, ct);
@@ -30,38 +33,14 @@ int findbestspoty(int currx, int curry, int &bestx, int &besty, GameBoard *game,
 	if(ct >= oldct){
 		bestx = currx;
 		besty = curry;
+		oldct = ct;
+	}
+	if(curry + 1 == game-> size){
+		return oldct;
 	}
 	findbestspoty(currx, curry + 1, bestx, besty, game, oldct);
-	return 0;
 
 }
-
-
-
-
-
-
-
-/*
-int findbestspoty(int currx, int curry, int &bestx, int &besty, GameBoard *game, int oldct){
-	&newx = currx;
-	&newy = curry;
-	if(game(currx)(curry) == '-'){
-		if(game(currx-1)(curry) == 'O'){
-			&currcount++;
-
-			findbestspoty(currx-1,curry, *bestx, *besty, &game, oldct);
-		}
-		if(game(currx-1)(curry) == 'X'){
-			if(&currcount > oldct){
-				oldct = &currcount;
-				&bestx = currx;
-				&besty = curry;
-			}
-
-	}
-}
-*/
 
 
 
